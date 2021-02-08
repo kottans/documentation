bdda0fadf9664ed751f5654b753d64eb04982528
OleksiyRudenko <oleksiy.rudenko@gmail.com>
2021-02-08 23:41:39 +0200

    Add docs page: Open Mic

--- sidebars.js
+++ sidebars.js
@@ -6,7 +6,8 @@ module.exports = {
       'doc/code-review',
       'doc/philosophy',
       'doc/kottans-course-bootstrap',
-      'doc/ongoing-project',],
+      'doc/ongoing-project',
+      'doc/open-mic-uk',],
 
     Materials: ['courses_materials/chats', 'courses_materials/github', 'courses_materials/youtube',],
   },
